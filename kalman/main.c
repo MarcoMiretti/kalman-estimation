e6eeb414399b50224333c4fa15a1e58213738c1a
MarcoMiretti <marco.miretti@gmail.com>
2017-12-15 11:53:34 -0300

    Utilización de la entrada A15 (P6.0) para lectura analógica.

--- kalman/main.c
+++ kalman/main.c
@@ -29,7 +29,7 @@ void main(void)
     ADC14->CTL0 = ADC14_CTL0_SHT0_2 | ADC14_CTL0_SHP | ADC14_CTL0_ON;
     ADC14->CTL1 = ADC14_CTL1_RES_2;         // Use sampling timer, 12-bit conversion results
 
-    ADC14->MCTL[0] |= ADC14_MCTLN_INCH_1;   // A1 ADC input select; Vref=AVCC
+    ADC14->MCTL[0] |= ADC14_MCTLN_INCH_15;   // A15 ADC input select; Vref=AVCC
     ADC14->IER0 |= ADC14_IER0_IE0;          // Enable ADC conversion complete interrupt
 
     SCB->SCR &= ~SCB_SCR_SLEEPONEXIT_Msk;   // Wake up on exit from ISR
@@ -47,7 +47,7 @@ void main(void)
 
 // ADC14 interrupt service routine
 void ADC14_IRQHandler(void) {
-    if (ADC14->MEM[0] >= 0x7FF)             // ADC14MEM0 = A1 > 0.5AVcc?
+    if (ADC14->MEM[0] >= 0x7FF)             // ADC14MEM0 = A15 > 0.5AVcc?
       P1->OUT |= BIT0;                      // P1.0 = 1
     else
       P1->OUT &= ~BIT0;                     // P1.0 = 0
